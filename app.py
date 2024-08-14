3645c6e8bad5aa7c81bbdbe61d1a9d47ac545ae0
pernebay-arailym <pernebayarailym@gmail.com>
2024-08-14 10:24:07 +0200

    downloaded the data csv,  libs, render

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 # How to Build, Deploy & Share a Python app!
+from pathlib import Path
+import pandas as pd
+
 import plotly.express as px
-from shiny.express import input, ui
+from shiny import reactive
+from shiny.express import render,input, ui
 from shinywidgets import render_plotly
 
 ui.page_opts(title="Sales Dashboard - Video 1 of 5", fillable=True)
+
+@reactive.calc
+def dat():
+    infile = Path(__file__).parent / "data/sales.csv"
+    return pd.read_csv(infile)
+
 with ui.layout_columns():
 
     @render_plotly
